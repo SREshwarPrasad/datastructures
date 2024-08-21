f6b9cd63d02a350b6a7f3877c2158c022ff5384a
S R Eshwar Prasad <141043413+SREshwarPrasad@users.noreply.github.com>
2024-08-21 19:41:56 +0530

    LL.cpp

--- LL.cpp
+++ LL.cpp
@@ -1,4 +1,5 @@
-//Stack using linked list
+//STACK USING LINKED LIST
+
 #include<iostream>
 using namespace std;
 struct node
@@ -82,3 +83,112 @@ int main()
   	
   	return 0;
 }
+
+_______________________________________________________________________________________________________
+
+
+//QUEUE USING LINKED LIST
+
+
+
+#include<iostream>
+using namespace std;
+
+struct node
+{
+	int val;
+	node *next;
+};
+struct node *f=NULL;
+struct node *r=NULL;
+
+void enqueue()
+{
+	struct node *temp;
+	temp=new node;
+	cin>>temp->val;
+	temp->next=NULL;
+	if(r==NULL && f==NULL)
+	{
+		f=temp;
+		r=temp;
+	}
+	else
+	{
+		r->next=temp; 
+		r=temp;
+	}
+}
+void dequeue()
+{
+	struct node *temp;
+	temp=f;
+	cout<<temp->val<<" deleted ";
+	f=f->next;
+	if (f==NULL)
+	{
+		r=NULL;
+	}
+	delete temp;
+}
+void print()
+{
+	struct node *temp;
+	for(temp=f; temp!=NULL;temp=temp->next)
+	{
+		cout<<temp->val<<"\t";
+	}
+}
+int main()
+{
+		int ch=1;
+	int c;
+	cout<<"Queue using Linked List"<<endl;
+	while(ch==1)
+	{	cout<<"1- enqueue 2-dequeue: ";
+		cin>>c;
+		switch(c)
+		{
+			case 1:
+			{
+				cout<<"Enter element to be pushed: ";
+				enqueue();
+				cout<<"Elements: ";
+				print();
+				break;
+			}
+			case 2:
+			{
+				dequeue();
+				cout<<"Elements ";
+				print();
+				break;
+			}
+			default:
+			{
+				cout<<"enter choice 1 or 2 ";
+			}
+		}
+		//print();
+		cout<<"\nproceed-1 quit-0 "; 
+		cin>>ch;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
